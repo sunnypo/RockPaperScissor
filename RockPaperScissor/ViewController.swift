82a16a8c8b6508efec2d8e6acf40c8724946391f
Sunny Huang <sunny208@me.com>
2019-01-06 10:16:14 -0500

    Initial Commit

---
+++ RockPaperScissor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RockPaperScissor
+//
+//  Created by Sunny on 2019/1/6.
+//  Copyright © 2019 supencil. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
